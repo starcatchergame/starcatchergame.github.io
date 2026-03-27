4ffe0549396a2626e998f087ed7f91b7fcd7c9ad
Gizmocow501 <larsonkeagan@gmail.com>
2026-03-27 17:32:55 -0500

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-window.onload = () => {
-    console.log("JS LOADED");
-
-    const floatingTitle = document.getElementById('floating-title');
-    console.log("floatingTitle:", floatingTitle);
-
-}
